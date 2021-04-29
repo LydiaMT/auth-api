749f6da865dcff8f8d8344f69bdcc5f3657c6895
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-04-28 19:48:43 -0700

    v2.js

--- src/error-handlers/500.js
+++ src/error-handlers/500.js
@@ -10,11 +10,17 @@
 module.exports = function (err, req, res, next) {
 
   // Sometimes, errors come in as an object, others as a string
-  const error = err.message ? err.message : err;
+  // const error = err.message ? err.message : err;
 
-  const errorObject = {
-    status: 500,
-    message: error
-  };
-  res.status(500).json(errorObject);
+  let error = { error: err.message || err };
+  // const errorObject = {
+  //   status: 500,
+  //   message: error
+  // };
+  res.statusCode = err.status || 500;
+  res.statusMessage = err.statusMessage || 'Server Error';
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify(error));
+  // res.status(500).json(errorObject);
+  res.end();
 };
--- src/server.js
+++ src/server.js
@@ -1,22 +1,40 @@
 'use strict';
 
+// 3rd Party Resources
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const notFoundHandler = require('./error-handlers/404.js');
+// Esoteric Resources
 const errorHandler = require('./error-handlers/500.js');
+const notFoundHandler = require('./error-handlers/404.js');
+
+const authRoutes = require('./routes/routes.js');
 const logger = require('./middleware/logger.js');
 
 const v1Routes = require('./routes/v1.js');
+// const v2Routes = require('./routes/v2.js');
 
+// Prepare the express app
 const app = express();
 
-app.use(express.json());
+// App Level MW
 
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(authRoutes);
 app.use(logger);
 
 app.use('/api/v1', v1Routes);
+// app.use('/api/v2', v2Routes);
 
 app.use('*', notFoundHandler);
+// app.use(notFound);
 app.use(errorHandler);
 
 module.exports = {
